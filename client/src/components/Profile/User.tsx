a21ba221702855508fb349b4f5c5aa7fed6aa5e1
TiennLevii <nguyentrachtien2401@gmail.com>
2023-11-30 23:58:50 +0700

    Update Folder Profile

--- client/src/components/Profile/User.tsx
+++ client/src/components/Profile/User.tsx
@@ -1,8 +1,65 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useInterceptors from "../../hooks/useInterceptors";
-import { FaUser, FaInfo, FaShieldAlt, FaPaperclip } from "react-icons/fa";
+import {
+  FaUser,
+  FaInfo,
+  FaShieldAlt,
+  FaPaperclip,
+  FaCreditCard,
+} from "react-icons/fa";
 import RefreshToken from "../../hooks/useRefreshToken";
+
+const tabViews = [
+  {
+    index: 1,
+    name: "Overview",
+    icon: FaUser,
+  },
+  {
+    index: 2,
+    name: "Detail",
+    icon: FaInfo,
+  },
+  {
+    index: 3,
+    name: "Security",
+    icon: FaShieldAlt,
+  },
+  {
+    index: 4,
+    name: "Connection",
+    icon: FaPaperclip,
+  },
+  {
+    index: 5,
+    name: "Payment Methods",
+    icon: FaCreditCard,
+  },
+];
+
+const tabViewInformation = [
+  {
+    index: 1,
+    name: "Overview",
+  },
+  {
+    index: 2,
+    name: "Detail",
+  },
+  {
+    index: 3,
+    name: "Security",
+  },
+  {
+    index: 4,
+    name: "Connection",
+  },
+  {
+    index: 5,
+    name: "Payment Methods",
+  },
+];
 
 function User() {
   const [tab, setTab] = useState<number>(1);
@@ -25,9 +82,7 @@ function User() {
         setInfor(response.data.user);
         console.log(response.data.user);
       } catch (err) {
-        // navigate("/");
-        // localStorage.removeItem("RefreshToken");
-        // localStorage.removeItem("AccessToken");
+        navigate("/");
         console.log(err);
       }
     };
@@ -55,72 +110,20 @@ function User() {
         <p onClick={refresh}>Refresh Token</p>
         <div className="flex justify-between">
           <div className="w-[380px] h-[450px] bg-F7F7F7">
-            <div
-              onClick={() => setTab(1)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 1
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaUser className="text-[21px] ml-2" />
-              <p className={"text-[19px] ml-2"}>Overview</p>
-            </div>
-            <div
-              onClick={() => setTab(2)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 2
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaInfo className="text-[21px] ml-2" />
-              <p className="text-[19px] ml-2">Detail</p>
-            </div>
-            <div
-              onClick={() => setTab(3)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 3
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaShieldAlt className="text-[21px] ml-2" />
-              <p className="text-[19px] ml-2">Security</p>
-            </div>
-            <div
-              onClick={() => setTab(4)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 4
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaPaperclip className="text-[21px] ml-2" />
-              <p className="text-[19px] ml-2">Connection</p>
-            </div>
-            <div
-              onClick={() => setTab(5)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 5
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaUser className="text-[21px] ml-2" />
-              <p className="text-[19px] ml-2">Game </p>
-            </div>
-            <div
-              onClick={() => setTab(6)}
-              className={`flex items-center p-4 cursor-pointer ${
-                tab === 6
-                  ? "border-r-[5px] border-blue bg-bluethird text-white"
-                  : ""
-              } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
-            >
-              <FaUser className="text-[21px] ml-2" />
-              <p className="text-[19px] ml-2">Payment Methods</p>
-            </div>
+            {tabViews.map((item) => (
+              <div
+                key={item.index}
+                onClick={() => setTab(item.index)}
+                className={`flex items-center p-4 cursor-pointer ${
+                  tab === item.index
+                    ? "border-r-[5px] border-blue bg-bluethird text-white"
+                    : ""
+                } hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
+              >
+                <item.icon className="text-[21px] ml-2" />
+                <p className={"text-[19px] ml-2"}>{item.name}</p>
+              </div>
+            ))}
             <div
               className={`flex items-center p-4 cursor-pointer hover:border-r-[5px] hover:border-blue hover:bg-bluethird hover:text-white`}
               onClick={handleLogout}
