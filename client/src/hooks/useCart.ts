d01b1c4f82bf3b24cdb75c6f285280eceea38baf
TiennLevii <nguyentrachtien2401@gmail.com>
2024-01-06 22:45:48 +0700

    delete

--- client/src/hooks/useCart.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { CartContext } from "../components/Cart/CartProvider";
-
-function useCart() {
-  const context = useContext(CartContext);
-  return context;
-}
-
-export default useCart;
