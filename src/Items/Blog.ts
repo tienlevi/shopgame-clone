e4142ccfa5d1c78af462f4ffc3870cffa47ea43d
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-13 14:25:18 +0700

    test coomit

--- src/Items/Blog.ts
+++ src/Items/Blog.ts
@@ -5,33 +5,33 @@ interface Data {
 
 const BlogData: Data[] = [
   {
-    image: "../Asset/Image/Slide/istockphoto-1386173530-170667a.png",
+    image: "../../public/Asset/Image/Slide/istockphoto-1386173530-170667a.png",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
   {
-    image: "../Asset/Image/Slide/MWIII-REVEAL-FULL-TOUT.jpg",
+    image: "../../public/Asset/Image/Slide/MWIII-REVEAL-FULL-TOUT.jpg",
     text: "WORLDWIDE REVEAL: ANNOUNCING CALL OF DUTY: MODERN WARFARE III",
   },
   {
-    image: "../Asset/Image/Slide/istockphoto-1386173530-170667a.png",
+    image: "../../public/Asset/Image/Slide/istockphoto-1386173530-170667a.png",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
   {
     image:
-      "../Asset/Image/Slide/gameplaydeepdivefc24-pitchnotes-bootblack.png.adapt.crop16x9.431p.png",
+      "../../public/Asset/Image/Slide/gameplaydeepdivefc24-pitchnotes-bootblack.png.adapt.crop16x9.431p.png",
     text: "EA SPORTS FC™ 24 | Pitch Notes - Clubs Deep Dive",
   },
   {
-    image: "../Asset/Image/Slide/MWII-S04-FREE-ACCESS-TOUT.jpg",
+    image: "../../public/Asset/Image/Slide/MWII-S04-FREE-ACCESS-TOUT.jpg",
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
   {
     image:
-      "../Asset/Image/Slide/bf2042-s5-redux-article-featured-image-16x9.jpg.adapt.crop16x9.431p.jpg",
+      "../../public/Asset/Image/Slide/bf2042-s5-redux-article-featured-image-16x9.jpg.adapt.crop16x9.431p.jpg",
     text: "Battlefield Briefing - Development Update, August 2023",
   },
   {
-    image: "../Asset/Image/Slide/Y8S3_HeavyMettle_Thumbnail.png",
+    image: "../../public/Asset/Image/Slide/Y8S3_HeavyMettle_Thumbnail.png",
     text: "Rainbow Six Siege – Operation Heavy Mettle Operator and Gadget Guide",
   },
 ];
