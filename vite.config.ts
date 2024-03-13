1d80af973cdc02c7d55799d6b0d5cea28f4619d2
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-13 13:37:43 +0700

    fix not load img in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: [
+      ".tsx",
+      ".ts",
+      ".jsx",
+      ".js",
+      ".json",
+      ".png",
+      ".jpg",
+      ".jpeg",
+      ".gif",
+      ".svg",
+    ],
+  },
+});
